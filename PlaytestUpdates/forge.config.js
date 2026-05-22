4982e856237a51495b24bc5c66898d9bf91fe8fc
Bkstafford1971 <166080525+Bkstafford1971@users.noreply.github.com>
2026-05-22 13:24:14 -0500

    Add files via upload

--- PlaytestUpdates/forge.config.js
+++ PlaytestUpdates/forge.config.js
@@ -4,14 +4,14 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
-   // icon: './bloodspireshortcut',
+    icon: './bloodspireshortcut',
   },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
       config: {
-     //   setupIcon: './bloodspireshortcut.ico',
+        setupIcon: './bloodspireshortcut.ico',
         loadingGif: './Bloodspire.png',
       },
     },
